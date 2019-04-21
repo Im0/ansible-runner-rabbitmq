e336554aba7bdc64acb15c667c9553a1bddfe44a
John Imison <imoimo@gmail.com>
2019-04-21 19:49:33 +1000

    Updates and tested with pika 1.0.1 and ansible-runner 1.3.2

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ansible-runner-rabbitmq
 
 A very quick test of bringing together ansible-runner and pika for executing jobs.
+
+# Requirements
+
+ansible           2.7.10
+ansible-runner    1.3.2
+pika              1.0.1
+
--- test_ad_hoc_and_rabbit_sub.py
+++ test_ad_hoc_and_rabbit_sub.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import ansible_runner
 import pika
 
@@ -20,13 +21,18 @@ def on_message(channel, method_frame, header_frame, body):
     print(method_frame.delivery_tag)
     print(body)
     print()
-    exec_ansible_runner()
+    exec_ansible_runner(body)
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
-def exec_ansible_runner():
+def exec_ansible_runner(body):
     # Use private_data_dir if you want the output of the ansible run saved
     #r = ansible_runner.run(private_data_dir='/tmp/demo', host_pattern='localhost', module='shell', module_args='whoami')
-    r = ansible_runner.run(json_mode=True, host_pattern='localhost', module='shell', module_args='whoami')
+    #r = ansible_runner.run(json_mode=True, host_pattern='localhost', module='shell', module_args='whoami')
+    #
+    # Note: In ansible-runner 1.3.2 passing extravars creates a local directory and file: env/extravars
+    #       This file contains the passed extra vars, and, is not tidied up after the execution.
+    r = ansible_runner.run(json_mode=True, host_pattern='localhost', private_data_dir='./', 
+                           playbook='test.yml', extravars={'msg': body})
     print("{}: {}".format(r.status, r.rc))
     # successful: 0
     for each_host_event in r.events:
@@ -35,11 +41,11 @@ def exec_ansible_runner():
     print(r.stats)
 
 def main():
-    url = 'amqp://guest:guest@192.168.0.x:5672/%2F'
+    url = 'amqp://guest:guest@192.168.0.32:5672/%2F'
     parameters = pika.URLParameters(url)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.basic_consume(on_message, 'hello')
+    channel.basic_consume('hello', on_message)
     try:
         channel.start_consuming()
     except KeyboardInterrupt:
